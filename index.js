da04348ea5b89afb54ab2a03abdee80b66ea0cd1
mohamed1408 <51641929+mohamed1408@users.noreply.github.com>
2023-07-23 23:39:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser')
-
+const moment = require('moment')
 const path = require('path');
 const sql = require('mssql');
+const retaildb = require('mssql')
 const connstr = "Data Source=biz1server.database.windows.net;Initial Catalog=biz1pos;Persist Security Info=True;User ID=dbadmin;Password=B1zd0m##;MultipleActiveResultSets=True"
-
+const retailconnstr = "Server=tcp:biz1server.database.windows.net,1433;Initial Catalog=biz1pos;Persist Security Info=False;User ID=dbadmin;Password=B1zd0m##;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 app.use(bodyParser.json());
 app.use('/assets', express.static('assets'))
 
@@ -23,6 +24,34 @@ app.get('/', (req, res) => {
     });
 })
 
+const retaildbqr = (data, config) => {
+    retaildb.connect(retailconnstr, (err) => {
+        if(err) {return}
+        var req = new retaildb.Request();
+        let datetime = new Date()
+        datetime.setDate(1)
+        datetime.setMonth(new Date(config.date).getMonth()+1)
+        datetime = moment(datetime).format('YYYY-MM-DDThh:mm')
+        let temptable  = ``
+        data.forEach((dt, i) => {
+            temptable += `select ${i+1} i, '${dt.product}' name, ${dt.price} price, ${dt.amount} amount, ${dt.gst} gst ${i==0 ? 'into #sampledata' : ''}\n`
+            if(i < (data.length-1))
+                temptable += `union all\n`
+        })
+        // console.log(temptable)
+        let query = `
+            exec dbo.datapopulate
+        `
+        req.query(temptable + query, (err2, recordset) => {
+            if(err2) {
+                console.log(err2)
+                return
+            }
+            console.log(recordset)
+        })
+    })
+}
+
 app.post('/generatereport', (req, res) => {
     console.log(req.body)
     const b = req.body
@@ -36,7 +65,8 @@ app.post('/generatereport', (req, res) => {
                 console.log(`exec dbo.qwertyuiop ${b["0"]} ${b["5"]} ${b["12"]} ${b["18"]}`, err2)
                 res.send({status: 0, data: [], msg: "failed to execute query."})
             }
-            res.send({status: 200, msg: "success", data: recordset.recordset})
+            retaildbqr(recordset.recordset, b)
+            res.send({status: 200, msg: "success", data: recordset.recordset, b})
         })
     })
 })
